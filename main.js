be83f1674917c93f5627454cadaced2de684754b
JooLund <joona.lundberg2@gmail.com>
2020-01-10 19:49:33 +0200

    removed cors from package.json

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-
 const routes = require('./routes/routes')
 const port = process.env.PORT || 3005;
-
-//const cors = require('cors');
-//app.use(cors());
-//app.use(cors({ origin:true, credentials:true }));
 
 
 app.use(bodyParser.json());
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
 //Routing
 app.use('/api', routes);
 
